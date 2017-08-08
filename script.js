2616448d1eca939b7a6c04ed1039cd781b8b27fa
Lucas Chescheir <lucaschescheir@gmail.com>
2017-08-08 12:12:04 -0400

    draft

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ fetch("http://jservice.io/api/random")
   .then(
     function(response) {
       response.json().then(function(data) {
+        console.log(data);
         let questionBox = document.getElementById('questionText');
+        let scoreBox = document.getElementById('score')
         question =`<p> ${data[0].question} </p>`;
+        score = `<p> ${data[0].value}</p>`;
+        scoreBox.innerHTML = score;
         questionBox.innerHTML = question;
        });
        });
