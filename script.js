9b84cc880bf3eba70a789e0d58d7c63af0539911
Lucas Chescheir <lucaschescheir@gmail.com>
2017-08-08 11:37:28 -0400

    fixed quetion

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-console.log("hello");
+
 fetch("http://jservice.io/api/random")
   .then(
     function(response) {
       response.json().then(function(data) {
-        console.log(data);
-      })
-    });
-let questionBox = document.getElementById('questionText');
-question =`<p>${data[0].question}</p>`;
+        let questionBox = document.getElementById('questionText');
+        question =`<p> ${data[0].question} </p>`;
+        questionBox.innerHTML = question;
+       });
+       });
+// let questionBox = document.getElementById('questionText');
+// question =`<p> ${[response[0].question} </p>`;
+// questionBox.innerHTML = question;
