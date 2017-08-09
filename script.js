74ab97b551f744579da9026f5ad309bb728cdd45
Lucas Chescheir <lucaschescheir@gmail.com>
2017-08-08 20:52:51 -0400

    changed syle

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
 let questionBox = document.getElementById('questionText');
 
-let scoreBox = document.getElementById('score')
+let scoreBox = document.getElementById('points')
 
 let catagoryBox = document.getElementById('catagory');
+
+let submitBox = document.getElementById('submit');
+
+let answerBox = document.getElementById('answer');
+
+let correctAnswer = document.getElementById('correctAnswer');
 
 // big ole' function...
 fetch("http://jservice.io/api/random")
@@ -14,11 +20,14 @@ fetch("http://jservice.io/api/random")
         question =`<p> ${data[0].question} </p>`;
         score = `${data[0].value}`;
         catagory = `${data[0].category.title}`;
+        totalScore = score + scoreBox.innerHTML;
         scoreBox.innerHTML = score;
         questionBox.innerHTML = question;
-        catagoryBox.innerHTML = catagory
+        catagoryBox.innerHTML = catagory;
+        scoreBox.innerHTML = totalScore
        });
        });
+
 // let questionBox = document.getElementById('questionText');
 // question =`<p> ${[response[0].question} </p>`;
 // questionBox.innerHTML = question;
