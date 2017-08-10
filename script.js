5e097a59c84901f30e3903132f7f2dfe359ed25d
Lucas Chescheir <lucaschescheir@gmail.com>
2017-08-10 13:35:50 -0400

    just javascript

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let answerBox = document.getElementById('answer');
 
 let correctAnswer = document.getElementById('correctAnswer');
 
+let playerScore = document.getElementById('score');
+
 // big ole' function...
 fetch("http://jservice.io/api/random")
   .then(
@@ -20,12 +22,23 @@ fetch("http://jservice.io/api/random")
         question =`<p> ${data[0].question} </p>`;
         score = `${data[0].value}`;
         catagory = `${data[0].category.title}`;
-        //Added totalScore here, doesnt do anything but will need to be linked to submit buttin to cal. players total score
-        totalScore = score + scoreBox.innerHTML;
+        answer = `${data[0].answer}`;
+        totalScore = playerScore + scoreBox;
+        rightAnswer = `${data[0].answer}`;
+        //playerScore.innerHTML = score;
+        //scorebox.innerHTML = totalScore;
         scoreBox.innerHTML = score;
         questionBox.innerHTML = question;
         catagoryBox.innerHTML = catagory;
-        scoreBox.innerHTML = totalScore
+        //scoreBox.innerHTML = playerScore;
+        correctAnswer.innerHTML = rightAnswer;
+        playerScore.innerHTML += score;
+  // If statment for right/ wrong answer
+if(rightAnswer === answerBox.innerHTML){
+  score += totalScore;
+
+} else{playerScore }
+
        });
        });
 
