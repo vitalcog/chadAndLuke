085a5b37f8078170f733fa263679b46184e5839e
Lucas Chescheir <lucaschescheir@gmail.com>
2017-08-10 22:00:00 -0400

    fixed some buttons

--- script.js
+++ script.js
@@ -21,11 +21,12 @@ let totalScore = 0;
 answerBox.textContent = "";
 // big ole' function...
 function nextP() {
-
+console.log(answerBox.value);
+console.log(correctAnswer.innerHTML)
   fetch("http://jservice.io/api/random")
     .then(
       function(response) {
-        
+
         response.json().then(function(data) {
 
           console.log(data);
@@ -37,22 +38,28 @@ function nextP() {
           scoreBox.innerHTML = score;
           questionBox.innerHTML = question;
           catagoryBox.innerHTML = catagory;
-          correctAnswer.innerHTML = rightAnswer;
+
         });
       });
 }
-//submit button, cant get this to += to get total play score, also needs to compare answerBox to the right //anwser and display right or wrong
-
-//submitBox.addEventListener('click', function( {
-
+//submit button, cant get this to += to get total play score.
+//Its comparing the two but because we are entering the text i dont its reading it correctly, maybe its //location of code... I dunno.
+  submitBox.addEventListener('click', function() {
+  if(answerBox.value === correctAnswer.innerHTML){
+    return resultButton.innerHTML = 'Right!!'
+  }else {
+    return correctAnswer.innerHTML = rightAnswer;
+  }
+})
 //function that loads webpage once.
-
 nextP();
 
 
-//nextButton, class of next button to toggle, needs to clear answerBox and refresh question.
+//buttonNext is clearing the answerBox and refreshing the question, still acting a little wonky in other states
+// Its not clearing the correctAnswer either
+ buttonNext.addEventListener('click', function() {
+nextP()
+   return answerBox.value = '';
+    resultButton.innerHTML = '';
 
-buttonNext.addEventListener('click', function() {
-  nextP();
-  answerBox.innerHTML = 0;
-});
+ })
